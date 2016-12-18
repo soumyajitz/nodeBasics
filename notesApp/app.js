f3b7706bc1de595283315a9a1e5803d9d8387065
soumyajitz <2014soumyajit@gmail.com>
2016-12-17 19:02:43 -0600

    added package.json

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes');
 
 console.log(os.userInfo().username);
@@ -9,3 +11,10 @@ var note = notes.addNote();
 
 console.log(note);
 console.log(notes.addNumbers(2,3));
+console.log(_.isString('ekfmel'));
+
+var arr = [1,2,2,3,5,2,4,6,7,4,89,5];
+var filteredArray = _.uniq(arr);
+
+console.log(filteredArray);
+//console.log('Hello Nodemon');
